2447d1d00e23d64c0d5b667e8199118ecac1af21
AlexDeLara <hfdelara@gmail.com>
2023-01-24 20:53:21 -0600

    Add variable, referencing and interpolation

--- examples/main.tf
+++ examples/main.tf
@@ -1,3 +1,9 @@
+variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+    default = 8080
+}
+
 provider "aws" {
     region = "us-east-2"
 }
@@ -13,7 +19,7 @@ resource "aws_instance" "example" {
     user_data = <<-EOF
                 #!bin/bash
                 echo "Hello World!" > index.html
-                nohup busybox httpd -f -p 8080 &
+                nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
     vpc_security_group_ids = [aws_security_group.instance.id]
@@ -23,8 +29,8 @@ resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
 
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
